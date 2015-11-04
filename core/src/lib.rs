2b26a0382036f68738897c4634941a1c6073888e
Greg V <greg@unrelenting.technology>
2015-11-04 16:12:09 +0300

    add time, expand tuple into EncryptedEntry

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -5,9 +5,11 @@ extern crate rusterpassword;
 extern crate libc;
 extern crate libsodium_sys as sodium;
 extern crate sodiumoxide;
+extern crate chrono;
 
 use secstr::SecStr;
 use rusterpassword::gen_site_seed;
+use chrono::{DateTime, UTC};
 use cbor::{Encoder, Decoder, CborError};
 use libc::size_t;
 use std::collections::btree_map::{BTreeMap, Keys};
@@ -18,12 +20,21 @@ use sodiumoxide::crypto::secretbox::xsalsa20poly1305 as secbox;
 pub struct Vault {
     version: u16,
     padding: Vec<u8>,
-    entries: BTreeMap<String, (Vec<u8>, u32, Vec<u8>)> // mapping: name → nonce, counter, encrypted Entry
+    entries: BTreeMap<String, EncryptedEntry>
+}
+
+#[derive(PartialEq, Debug, RustcDecodable, RustcEncodable)]
+pub struct EncryptedEntry {
+    nonce: Vec<u8>,
+    counter: u32,
+    ciphertext: Vec<u8>
 }
 
 #[derive(PartialEq, Debug, RustcDecodable, RustcEncodable)]
 pub struct Entry {
-    fields: BTreeMap<String, Field>
+    fields: BTreeMap<String, Field>,
+    created_at: DateTime<UTC>,
+    updated_at: DateTime<UTC>
 }
 
 #[derive(PartialEq, Debug, RustcDecodable, RustcEncodable)]
@@ -68,34 +79,32 @@ pub type EntryResult<T> = Result<T, EntryError>;
 
 impl Vault {
 
-    pub fn entry_names(&self) -> Keys<String, (Vec<u8>, u32, Vec<u8>)> {
+    pub fn entry_names(&self) -> Keys<String, EncryptedEntry> {
         self.entries.keys()
     }
 
     pub fn get_entry(&self, entries_key: &SecStr, name: &str) -> EntryResult<Entry> {
-        if let Some(&(ref nonce, counter, ref ciphertext)) = self.entries.get(name) {
-            let nonce_wrapped = try!(secbox::Nonce::from_slice(&nonce).ok_or(EntryError::WrongNonceLength));
-            let entry_key_wrapped = try!(gen_entry_key(entries_key, name, counter));
-            let plaintext = SecStr::new(try!(secbox::open(&ciphertext, &nonce_wrapped, &entry_key_wrapped).map_err(|_| EntryError::DecryptionError)));
+        if let Some(ee) = self.entries.get(name) {
+            let nonce_wrapped = try!(secbox::Nonce::from_slice(&ee.nonce).ok_or(EntryError::WrongNonceLength));
+            let entry_key_wrapped = try!(gen_entry_key(entries_key, name, ee.counter));
+            let plaintext = SecStr::new(try!(secbox::open(&ee.ciphertext, &nonce_wrapped, &entry_key_wrapped).map_err(|_| EntryError::DecryptionError)));
             Ok(try!(try!(Decoder::from_bytes(plaintext.unsecure()).decode::<Entry>().next().ok_or(EntryError::DataError))))
         } else {
             Err(EntryError::EntryNotFound)
         }
     }
 
-    pub fn put_entry(&mut self, entries_key: &SecStr, name: &str, entry: &Entry) -> EntryResult<()> {
-        let counter = match self.entries.get(name) {
-            Some(&(_, counter, _)) => counter + 1,
-            _ => 1
-        };
+    pub fn put_entry(&mut self, entries_key: &SecStr, name: &str, entry: &mut Entry) -> EntryResult<()> {
+        entry.updated_at = UTC::now();
+        let counter = self.entries.get(name).map(|ee| ee.counter + 1).unwrap_or(1);
         let nonce_wrapped = secbox::gen_nonce();
         let secbox::Nonce(nonce) = nonce_wrapped;
         let entry_key_wrapped = try!(gen_entry_key(entries_key, name, counter));
         let mut e = Encoder::from_memory();
-        try!(e.encode(&[entry]));
+        try!(e.encode(&[&*entry]));
         let plaintext = SecStr::new(e.into_bytes());
         let ciphertext = secbox::seal(plaintext.unsecure(), &nonce_wrapped, &entry_key_wrapped);
-        self.entries.insert(String::from(name), (nonce.to_vec(), counter, ciphertext));
+        self.entries.insert(name.to_owned(), EncryptedEntry { nonce: nonce.to_vec(), counter: counter, ciphertext: ciphertext });
         Ok(())
     }
 
@@ -127,17 +136,18 @@ mod tests {
     use secstr::*;
     use rusterpassword::*;
     use std::collections::btree_map::BTreeMap;
+    use chrono::*;
 
     #[test]
     fn test_roundtrip_entry() {
         let mut fs = BTreeMap::new();
-        fs.insert(String::from("password"), Field::Derived { counter: 4, site_name: Some(String::from("twitter.com")), usage: DerivedUsage::Password(PasswordTemplate::Maximum) });
-        fs.insert(String::from("old_password"), Field::Stored { data: SecStr::from("h0rse"), usage: StoredUsage::Password });
-        let twitter = Entry { fields: fs };
+        fs.insert("password".to_owned(), Field::Derived { counter: 4, site_name: Some("twitter.com".to_owned()), usage: DerivedUsage::Password(PasswordTemplate::Maximum) });
+        fs.insert("old_password".to_owned(), Field::Stored { data: SecStr::from("h0rse"), usage: StoredUsage::Password });
+        let mut twitter = Entry { fields: fs, created_at: UTC::now(), updated_at: UTC::now() };
         let mut vault = Vault { version: 0, padding: b"".to_vec(), entries: BTreeMap::new() };
         let master_key = gen_master_key(SecStr::from("Correct Horse Battery Staple"), "Clarke Griffin").unwrap();
         let entries_key = gen_entries_key(&master_key);
-        vault.put_entry(&entries_key, "twitter", &twitter).unwrap();
+        vault.put_entry(&entries_key, "twitter", &mut twitter).unwrap();
         assert!(vault.get_entry(&entries_key, "twitter").unwrap() == twitter);
     }
 }
