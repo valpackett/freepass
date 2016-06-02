be4b98f911656bafcae7818550ea8c199a0f700b
Greg V <greg@unrelenting.technology>
2016-06-02 16:19:05 +0300

    android update stuff and add opening vaults

--- android/app/src/main/kotlin/technology/unrelenting/freepass/LoginActivity.kt
+++ android/app/src/main/kotlin/technology/unrelenting/freepass/LoginActivity.kt
@@ -1,31 +1,74 @@
 package technology.unrelenting.freepass
 
-import android.content.Context
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.InputType
 
 import org.jetbrains.anko.*
 
 class LoginActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Vault.freepass_init()
-        verticalLayout {
-            padding = dip(30)
-            editText {
-                hint = "Name"
-                textSize = 24f
-            }
-            editText {
-                hint = "Password"
-                textSize = 24f
-            }
-            button("Login") {
-                textSize = 26f
-            }
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		Vault.freepass_init()
+		verticalLayout {
+			padding = dip(30)
+			val name = editText {
+				hint = "Name"
+				text = Editable.Factory.getInstance().newEditable("Test")
+				textSize = 24f
+			}
+			val password = editText {
+				hint = "Password"
+				textSize = 24f
+				text = Editable.Factory.getInstance().newEditable("test")
+				inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+			}
+			button("Login") {
+				textSize = 26f
+				onClick {
+					val permission = ActivityCompat.checkSelfPermission(ctx, Manifest.permission.READ_EXTERNAL_STORAGE)
+					if (permission == PackageManager.PERMISSION_GRANTED) {
+						storageReadAllowed = true
+					} else {
+						ActivityCompat.requestPermissions(this@LoginActivity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+					}
+					if (!storageReadAllowed) {
+						ctx.toast("You must allow reading files!")
+					} else {
+						async() {
+							try {
+								Vault.open(ctx.cacheDir, "${Environment.getExternalStorageDirectory().path}/testvault", name.text.toString(), password.text.toString())
+							} catch (e: Exception) {
+								uiThread {
+									ctx.toast("Error: ${e.message}")
+									e.printStackTrace()
+								}
+							}
+							uiThread {
+								ctx.toast(if (Vault.isOpen) "Opened" else "Failed")
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	var storageReadAllowed = false
+
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+		for ((perm, res) in permissions.zip(grantResults.toList())) {
+			if (perm.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+				storageReadAllowed = res == PackageManager.PERMISSION_GRANTED
+			}
+		}
+	}
 
 }
--- android/app/src/main/kotlin/technology/unrelenting/freepass/Vault.kt
+++ android/app/src/main/kotlin/technology/unrelenting/freepass/Vault.kt
@@ -1,13 +1,79 @@
 import org.bytedeco.javacpp.*
 import org.bytedeco.javacpp.annotation.*
+import java.io.File
+
+class VaultException(reason: String) : Exception(reason) {}
 
 @Platform(library = "jniVault",
           cinclude = arrayOf("freepass_capi.h"))
 object Vault {
-    init {
-        Loader.load()
-        freepass_init()
-    }
+	var masterKey: Pointer? = null
+	var vaultObj: Pointer? = null
+	var entriesKey: Pointer? = null
+	var filePath: String? = null
+	var isOpen = false
 
-    @JvmStatic external fun freepass_init(): Unit
+	init {
+		Loader.load()
+		freepass_init()
+	}
+
+	fun open(cache_dir: File, path: String, username: String, password: String) {
+		if (isOpen) {
+			throw VaultException("Already open")
+		}
+		masterKey = rusterpassword_gen_master_key(password, username)
+		if (masterKey == null || masterKey!!.isNull) {
+			throw VaultException("Got null master key")
+		}
+		val outerKey = freepass_gen_outer_key(masterKey!!)
+		val entriesKey = freepass_gen_entries_key(masterKey!!)
+
+		val file = File(path)
+		vaultObj = if (file.exists()) {
+			if (!file.canRead()) {
+				throw VaultException("Not allowed to read file ${file.absolutePath}")
+			}
+			freepass_open_vault(file.absolutePath, entriesKey, outerKey)
+		} else {
+			freepass_new_vault(entriesKey, outerKey)
+		}
+		freepass_free_outer_key(outerKey)
+
+		if (vaultObj == null || vaultObj!!.isNull) {
+			rusterpassword_free_master_key(masterKey!!)
+			freepass_free_entries_key(entriesKey)
+			throw VaultException("Got null vault")
+		}
+
+		filePath = file.absolutePath
+		isOpen = true
+	}
+
+	fun close() {
+		if (masterKey != null) {
+			rusterpassword_free_master_key(masterKey!!)
+		}
+		masterKey = null
+		if (entriesKey != null) {
+			freepass_free_entries_key(entriesKey!!)
+		}
+		entriesKey = null
+		if (vaultObj != null) {
+			freepass_close_vault(vaultObj!!)
+		}
+		vaultObj = null
+		isOpen = false
+	}
+
+	@JvmStatic external fun freepass_init(): Unit
+	@JvmStatic external fun rusterpassword_gen_master_key(username: String, password: String): Pointer
+	@JvmStatic external fun rusterpassword_free_master_key(@Cast("secstr_t*") master_key: Pointer): Unit
+	@JvmStatic external fun freepass_gen_outer_key(@Cast("secstr_t*") master_key: Pointer): Pointer
+	@JvmStatic external fun freepass_gen_entries_key(@Cast("secstr_t*") master_key: Pointer): Pointer
+	@JvmStatic external fun freepass_free_outer_key(@Cast("secstr_t*") outer_key: Pointer): Unit
+	@JvmStatic external fun freepass_free_entries_key(@Cast("secstr_t*") entries_key: Pointer): Unit
+	@JvmStatic external fun freepass_open_vault(path: String, @Cast("secstr_t*") entries_key: Pointer, @Cast("secstr_t*") outer_key: Pointer): Pointer
+	@JvmStatic external fun freepass_new_vault(@Cast("secstr_t*") entries_key: Pointer, @Cast("secstr_t*") outer_key: Pointer): Pointer
+	@JvmStatic external fun freepass_close_vault(@Cast("vault_t*") vault: Pointer): Unit
 }
